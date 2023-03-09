222cb043603457a24c661e70c0d7ad0e239fd047
niloccolinus <76599809+niloccolinus@users.noreply.github.com>
2023-03-09 22:50:00 +0100

    Update CaesarCipher.cpp

--- CaesarCipher/CaesarCipher.cpp
+++ CaesarCipher/CaesarCipher.cpp
@@ -1,12 +1,24 @@
 
 #include <iostream>
 #include "Crypto.h"
+#include <windows.h>
 
 using namespace std;
 
+
 int main()
 {
+    // modifier la locale pour pouvoir afficher les caractères spéciaux (caractères français)
+    std::locale::global(locale(""));
     Crypto crypto;
-    crypto.Chiffrer(3);
-    crypto.Dechiffrer(3);
+
+    /* Permet de rester dans le programme jusqu'à ce que l'on décide de le quitter */
+    bool isRunning;
+
+    do
+    {
+        isRunning = crypto.Init(); // lancement du programme qui renverra 'vrai' une fois terminé
+    } while (!isRunning); // Tant que isRunning n'est pas 'vrai', on reste dans le programme
+
+
 }
